fdfec46e9e302d2a2a2f69c0bf0467bc1430b327
pedrovito <pedro.v.oliveira007@gmail.com>
2023-10-30 14:58:57 -0300

    recebendo paramentros na rota

--- node_express/index.js
+++ node_express/index.js
@@ -4,6 +4,13 @@ const path = require("path")
 
 const caminhoBase =path.join(__dirname, "templetes")
 
+app.get ( '/usuarios/:id', (resquisicao , resposta) =>{
+    const id = resquisicao.params.id
+    
+    console.log(`acessando dados do usuarios ${id}`)
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get ( '/' , (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
     //resposta.send("test nodemon")
