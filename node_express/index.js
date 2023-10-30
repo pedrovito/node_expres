9eb27de65d6bc355fc0ef493317bfe0163e8fddb
pedrovito <pedro.v.oliveira007@gmail.com>
2023-10-30 14:45:40 -0300

    pedro teste terminado

--- node_express/index.js
+++ node_express/index.js
@@ -1,10 +1,15 @@
 const express =require("express")
 const app=express()
+const path = require("path")
+
+const caminhoBase =path.join(__dirname, "templetes")
 
 app.get ( '/' , (requisicao, resposta) => {
-    resposta.send("estou funcionando com express")
+    resposta.sendFile(`${caminhoBase}/index.html`)
+    //resposta.send("test nodemon")
+    console.log(caminhoBase)
 })
 
 app.listen(3000, () => {
     console.log("servido rodando  na porta 3000")
-})
+})  
