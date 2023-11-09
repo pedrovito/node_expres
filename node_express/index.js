f2fdd294e3ebdb3130cd0214fe1db1a100507802
pedrovito <pedro.v.oliveira007@gmail.com>
2023-11-09 15:51:44 -0300

    recebendo  informação do formulario no corpo da resquisição

--- node_express/index.js
+++ node_express/index.js
@@ -1,8 +1,28 @@
+const exp = require("constants")
+const { json } = require("express")
 const express =require("express")
 const app=express()
 const path = require("path")
 
 const caminhoBase =path.join(__dirname, "templetes")
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.post('/cadastro/salvar', (requisicao ,resposta) =>{
+    const nome = requisicao.body.nome
+    const email = requisicao.body.email
+    const senha= requisicao.body.senha
+    
+    console.log(` o email do usuario :${email}`)
+})
+
+app.get('/cadastrar', (requisicao , resposta) =>{
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
+})
 
 app.get ( '/usuarios/:id', (resquisicao , resposta) =>{
     const id = resquisicao.params.id
